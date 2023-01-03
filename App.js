4f76861b605a356f83e0a5d80756eba7de2c9dce
Ravi Kumar <ravi.rks4488@gmail.com>
2023-01-03 17:16:40 +0530

    _TicTacToe

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import bg from "./assets/bg.jpeg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
+        <View style={styles.circle} />
+
+        <View>
+          <View style={styles.crossLine}/>
+          <View style={[styles.crossLine,styles.crossLineR]}/>
+        </View>
+
+      </ImageBackground>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#242D34",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  bg: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 15,
+  },
+  circle: {
+    width: 75,
+    height: 75,
+    borderColor: "white",
+    borderRadius: 50,
+    borderWidth:10,
+    alignItems: "center",
+    justifyContent: "center",
+    margin:10,
+
+  },
+  crossLine:{
+    position:'absolute',
+    width:11,
+    height:95,
+    backgroundColor:'white',
+    transform:[
+      {
+        rotate: '45deg'
+      }
+    ]
+  },
+  crossLineR:{
+    transform:[
+      {
+        rotate: '-45deg'
+      }
+    ]
+  }
 });
