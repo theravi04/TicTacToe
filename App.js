09c674216929f01d45a07aa7db3df36ce5edbfe3
Ravi Kumar <ravi.rks4488@gmail.com>
2023-01-04 20:57:05 +0530

    Day_2

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from "expo-status-bar";
+import React, {useState} from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import bg from "./assets/bg.jpeg";
 
 export default function App() {
+
+  const [map, setMap] = useState([
+
+    ['o','',''],  //1st Row
+    ['','x','x'],  //2st Row
+    ['o','',''],  //3st Row
+
+  ]);
+
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <View style={styles.circle} />
+        <View style={styles.map}>
 
-        <View>
-          <View style={styles.crossLine}/>
-          <View style={[styles.crossLine,styles.crossLineR]}/>
+          {map.map(row => {
+            row.map(cell =>(
+              <View style={styles.cell}></View>
+            ))
+          })}
+
+          {/* <View style={styles.circle} />
+          <View style={styles.cross}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineR]} />
+          </View> */}
+
         </View>
-
       </ImageBackground>
 
       <StatusBar style="auto" />
@@ -34,33 +53,57 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: 15,
   },
+  map:{
+    borderWidth:1,
+    borderColor:'white',
+    width:'80%',
+    aspectRatio:1,
+  },
+  cell:{
+    
+  },
   circle: {
+    position:'absolute',
+
+    left: 1 * 110,
+    top:2 * 110,
+
     width: 75,
     height: 75,
     borderColor: "white",
     borderRadius: 50,
-    borderWidth:10,
+    borderWidth: 10,
     alignItems: "center",
     justifyContent: "center",
-    margin:10,
+    margin: 10,
+  },
+  cross:{
 
-  },
-  crossLine:{
+    // left: 0 * 119,
+    // top: 0 * 117,
+
+    width: 75,
+    height: 75,
     position:'absolute',
-    width:11,
-    height:95,
-    backgroundColor:'white',
-    transform:[
-      {
-        rotate: '45deg'
-      }
-    ]
   },
-  crossLineR:{
-    transform:[
+  crossLine: {
+    position: "absolute",
+    left:32.5,
+    width: 10,
+    height: 70,
+    backgroundColor: "white",
+    borderRadius:5,
+    transform: [
       {
-        rotate: '-45deg'
-      }
-    ]
-  }
+        rotate: "45deg",
+      },
+    ],
+  },
+  crossLineR: {
+    transform: [
+      {
+        rotate: "-45deg",
+      },
+    ],
+  },
 });
