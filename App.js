004f676f20c184517bdd26b26c9a6fa9c4bef3ff
Ravi Kumar <ravi.rks4488@gmail.com>
2023-02-09 20:18:36 +0530

    Win

--- App.js
+++ App.js
@@ -10,12 +10,14 @@ import {
 } from "react-native";
 import bg from "./assets/bg.jpeg";
 
-export default function App() {
-  const [map, setMap] = useState([
+const emptyMap = [
     ["", "", ""], //1st Row
     ["", "", ""], //2st Row
     ["", "", ""], //3st Row
-  ]);
+]
+
+export default function App() {
+  const [map, setMap] = useState();
 
   const [currentTurn, setCurrentTurn] = useState("o");
 
@@ -43,11 +45,11 @@ export default function App() {
       const isRowXWinning = map[i].every((cell) => cell == "x");
       const isRowOWinning = map[i].every((cell) => cell == "o");
       if (isRowXWinning) {
-        Alert.alert("X won, Row : ",i);
+        Alert.alert('X won, Row '); //check for row number
         // break;
       }
       if (isRowOWinning) {
-        Alert.alert("O won, Row : ",i);
+        Alert.alert('O won, Row '); //check for row number
         // break;
       }
     }
@@ -68,19 +70,59 @@ export default function App() {
       }
 
       if (isColumnXWinning) {
-        Alert.alert("X won, Col : ", col);
-        // break;
+        Alert.alert('X won. Col '); //check for column number
+        break;
       }
-      else {
-        Alert.alert("O won, Col : ", col);
-        // break;
+      else if(isColumnOWinning) {
+        Alert.alert('O won. Col '); //check for column number
+        break;
       }
     }
 
     //check diagonals
+    let isDiagonalO1Winning = true;
+    let isDiagonalX1Winning = true;
 
+    let isDiagonalO2Winning = true;
+    let isDiagonalX2Winning = true;
 
+    for(let i=0;i<3;i++){
+      if(map[i][i] != 'o'){
+        isDiagonalO1Winning = false;
+      }
+      if(map[i][i] != 'x'){
+        isDiagonalX1Winning = false;
+      }
+
+      if(map[i][2-i] != 'o'){
+        isDiagonalO2Winning = false;
+      }
+      if(map[i][2-i] != 'x'){
+        isDiagonalX2Winning = false;
+      }
+    }
+
+    if(isDiagonalO1Winning)
+      Alert.alert('O won. Diagonal 1');
+    
+    if(isDiagonalX1Winning)
+      Alert.alert('X won. Diagonal 1');
+
+    if(isDiagonalO2Winning)
+      Alert.alert('O won. Diagonal 2');
+    
+    if(isDiagonalX2Winning)
+      Alert.alert('X won. Diagonal 2');
   };
+
+  const gameWon = (player) =>{
+    Alert.alert(``);
+  }
+
+  const resetGame = () =>{
+    setMap(emptyMap);
+    setCurrentTurn("x");
+  }
 
   return (
     <View style={styles.container}>
